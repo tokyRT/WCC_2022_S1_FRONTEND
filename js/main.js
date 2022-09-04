66598077fee15bae0fb0b558ffb94881d95a9c1c
Toky Ratolojanahary <tokyrt.pro@gmail.com>
2022-09-04 18:14:55 +0300

    mouse animation

--- js/main.js
+++ js/main.js
@@ -8,12 +8,29 @@ window.addEventListener('load', function(){
 const toggleBtn = document.querySelector('.header .toggle-icon');
 const nav = document.querySelector('nav')
 toggleBtn.addEventListener('click', function(){
-    nav.style.left = 0;
+    nav.classList.add('show');
     document.body.style.overflowY = "hidden";
 });
 
 const closeNavBtn = document.querySelector('nav .close-icon');
 closeNavBtn.addEventListener('click', function(){
-    nav.style.left="110%";
+    nav.classList.remove('show');
     document.body.style.overflowY = "scroll";
+});
+
+let mouseX, mouseY;
+const floatings = document.querySelectorAll('.floating, .rectangle');
+document.addEventListener('mousemove', function(e){
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    // console.log(mouseX, mouseY);
+    for (let i = 0; i < floatings.length; i++) {
+        const el = floatings[i];
+        const elTop = el.getBoundingClientRect().top;
+        const elLeft = el.getBoundingClientRect().left;
+        let factorX =  mouseX/230;
+        let factorY =  mouseY/210;
+        el.style.transform = `translate(${Math.pow(-1, i) * factorX}px, ${Math.pow(-1, i) * factorY}px)`;
+    }
+    // console.log(floatings[0].getBoundingClientRect().left);
 })
